380a4e7815bdc1459deaa6c6467c998aacc773f3
Ammaar Doctor <ammaar.doctor@gmail.com>
2024-07-02 21:04:23 -0500

    finished adding sketch and dimension changes

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ addSquares(dim);
 function applyNthChild(step) {
     const squares = document.querySelectorAll('.square');
     squares.forEach((square, index) => {
-        if ((index)+1 % step === 0) { // index is zero-based, so we add 1
-            console.log(index.toString()+" % "+step.toString());
+        if ((index) + 1 % step === 0) { // index is zero-based, so we add 1
+            console.log(index.toString() + " % " + step.toString());
             square.style.marginLeft = "0";
             square.style.borderLeftWidth = "3px";
         }
@@ -19,13 +19,13 @@ let button = document.querySelector("button");
 button.addEventListener("click", (e) => {
     let dimval = 0;
 
-    while(dimval > 100 || dimval < 1) {
+    while (dimval > 100 || dimval < 1) {
         dimval = prompt("Enter dimension (100 max)");
         if (dimval === null) return;
         dimval = parseInt(dimval, 10);
 
     }
-    
+
     removeSquares();
     addSquares(dimval);
 });
@@ -40,12 +40,25 @@ function removeSquares() {
 function addSquares(dimen) {
     let container = document.querySelector(".container");
     container.innerHTML = '';
-    for(let i=0; i < dimen*dimen; i++) {
+    for (let i = 0; i < dimen * dimen; i++) {
         let sq = document.createElement("div");
         sq.classList.add("square");
         sq.style.width = `calc(100% / ${dimen})`;
         sq.style.paddingBottom = `calc((100% / ${dimen}) - 6px)`;
         container.appendChild(sq);
     }
+    AddSketching();
     applyNthChild(dimen);
 }
+
+//Etch the sketch
+function AddSketching() {
+    let sqs = document.querySelectorAll(".square");
+    sqs.forEach(square => {
+        square.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = "grey";
+        });
+    });
+}
+
+AddSketching();
